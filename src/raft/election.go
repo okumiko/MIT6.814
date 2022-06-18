c69beab65e5a0a122bfbe82da9562d794914fa9a
shangwenbin <shangwenbin@sensetime.com>
2022-06-18 14:03:20 +0800

    lab2c

--- src/raft/election.go
+++ src/raft/election.go
@@ -23,8 +23,24 @@ func (rf *Raft) StartElection() {
 				rf.mu.Lock()
 				defer rf.mu.Unlock()
 
-				//有锁，并不是并发执行的，不会有race
-				rf.handleRequestVoteReply(peer, grantedVotes, args, reply)
+				DPrintf("<Node %v> receives RequestVoteReply %v from <Node %v> after sending RequestVoteArgs %v in term %v", rf.me, reply, peer, args, rf.currentTerm)
+
+				//用rf.currentTerm == request.Term跳过过期的请求回复
+				if rf.currentTerm == args.Term && rf.state == StateCandidate {
+					if reply.VoteGranted { //回复同意投票
+						grantedVotes += 1                   //计数加一
+						if grantedVotes > len(rf.peers)/2 { //超过半数以上
+							rf.ToState(StateLeader) //本节点变为领导者
+
+							DPrintf("<Node %v> receives majority votes in term %v", rf.me, rf.currentTerm)
+						}
+					} else if reply.Term > rf.currentTerm { //回复不同意，并且收到的回复任期大于当前节点任期
+						DPrintf("<Node %v> finds a new leader <Node %v> with term %v and steps down in term %v", rf.me, peer, reply.Term, rf.currentTerm)
+						rf.currentTerm = reply.Term //更新本节点任期
+						rf.ToState(StateFollower)   //本节点变为跟随者
+						rf.persist()                //持久化
+					}
+				}
 			}
 		}(peer)
 	}
@@ -61,50 +77,28 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	rf.mu.Lock()
 	defer rf.mu.Unlock()
 	defer rf.persist() //持久化
-	defer DPrintf("{Node %v}'s state is {state %v,term %v,commitIndex %v,lastApplied %v,firstLog %v,lastLog %v} before processing requestVoteArgs %v and reply requestVoteReply %v", rf.me, rf.state, rf.currentTerm, rf.commitIndex, rf.lastApplied, rf.getFirstLog(), rf.getLastLog(), args, reply)
+	defer DPrintf("<Node %v>: {role %v,term %v,commitIndex %v,lastApplied %v,firstLog %v,lastLog %v} before processing requestVoteArgs %v and reply requestVoteReply %v", rf.me, rf.state.String(), rf.currentTerm, rf.commitIndex, rf.lastApplied, rf.getFirstLog(), rf.getLastLog(), args, reply)
 
 	//识别出过期的server，更新它的term
 	//1.term小；2.同一个任期接到了两个投票请求，voteFor说明已经投了一个票，如果投的不是之前投的就拒绝投票(一个term只能投一次票，要不然票数都乱了)
 	if args.Term < rf.currentTerm || (args.Term == rf.currentTerm && rf.votedFor != -1 && rf.votedFor != args.CandidateId) {
 		reply.Term, reply.VoteGranted = rf.currentTerm, false
-		DPrintf("here1")
 		return
 	}
+
 	//投票，变成跟随者
 	if args.Term > rf.currentTerm {
-		rf.ToState(StateFollower)  //当前节点改变状态
-		rf.currentTerm = args.Term //更新当前节点任期
+		rf.ToState(StateFollower) //当前节点改变状态
+		rf.currentTerm, rf.votedFor = args.Term, -1
 	}
+
 	if !rf.isLogUpToDate(args.LastLogTerm, args.LastLogIndex) {
 		reply.Term, reply.VoteGranted = rf.currentTerm, false
-		DPrintf("here2")
 		return
 	}
 	rf.votedFor = args.CandidateId
 	resetTimer(rf.electionTimer, RandomizedElectionTimeout()) //reset选举定时器
 	reply.Term, reply.VoteGranted = rf.currentTerm, true
-}
-
-//使用前加锁
-func (rf *Raft) handleRequestVoteReply(peer int, grantedVotes int, args *RequestVoteArgs, reply *RequestVoteReply) {
-	DPrintf("<Node %v> receives RequestVoteReply %v from <Node %v> after sending RequestVoteArgs %v in term %v", rf.me, reply, peer, args, rf.currentTerm)
-
-	//用rf.currentTerm == request.Term跳过过期的请求回复
-	if rf.currentTerm == args.Term && rf.state == StateCandidate {
-		if reply.VoteGranted { //回复同意投票
-			grantedVotes += 1                   //计数加一
-			if grantedVotes > len(rf.peers)/2 { //超过半数以上
-				rf.ToState(StateLeader) //本节点变为领导者
-
-				DPrintf("<Node %v> receives majority votes in term %v", rf.me, rf.currentTerm)
-			}
-		} else if reply.Term > rf.currentTerm { //回复不同意，并且收到的回复任期大于当前节点任期
-			DPrintf("<Node %v> finds a new leader <Node %v> with term %v and steps down in term %v", rf.me, peer, reply.Term, rf.currentTerm)
-			rf.currentTerm = reply.Term //更新本节点任期
-			rf.ToState(StateFollower)   //本节点变为跟随者
-			rf.persist()                //持久化
-		}
-	}
 }
 
 //用于投票时，投票者判断candidate的日志是否至少和接收者的日志一样新(up-to-date)
